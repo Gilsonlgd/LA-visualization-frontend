47d9639f37e121962a5e05dbe159f6830f5ca2b0
Gilson Garcia <garciadasilvagilson@gmail.com>
2023-08-03 20:19:27 -0300

    update: Add global styles

---
+++ src/GlobalStyles.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background: teal;
+    font-family: Open-Sans, Helvetica, Sans-Serif;
+    @import '~bootstrap/dist/css/bootstrap.min.css';
+  }
+`;
+
+export default GlobalStyle;
